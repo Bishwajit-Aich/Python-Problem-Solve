8f6ea24b4225d11c374c4da1597d71d2a2049f92
bishwajit870 <aichbishwajit75@gmail.com>
2023-12-28 00:49:52 +0600

    first commit

---
+++ comment.py
@@ -0,0 +1,8 @@
+#Single Line Comment Using #
+
+print("Single Line Comment Using #")
+
+
+'''Double Line Comment Using Six Cotation'''
+
+print('Double Line Commeni using six cotation')
---
+++ print.py
@@ -0,0 +1,1 @@
+print('Bishwajit Aich')
