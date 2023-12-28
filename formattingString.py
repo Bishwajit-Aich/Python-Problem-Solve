691a4fe32ebed911858844f917f0eed598f43d8d
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-12-28 12:08:42 +0600

    Formatting string easily can added anything

---
+++ formattingString.py
@@ -0,0 +1,9 @@
+num1 = 540;
+num2 = 60;
+
+print(f"The Total Number is {num1 + num2}")
+
+YourNameIs = "Bishwajit Aich"
+YourAddressIs = "Mayor Gali."
+
+print(f"My name is {YourNameIs} Also My Address is {YourAddressIs}")
