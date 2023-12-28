83a6bcb9fa85caadbed59631b0f17a6985c63a95
Bishwajit Aich <84278738+Bishwajit-Aich@users.noreply.github.com>
2023-12-28 12:02:17 +0600

    Python Datatype.Ex. int,float,bool,string

---
+++ datatype.py
@@ -0,0 +1,41 @@
+# how to find integer number
+intNumber = 10
+
+print(type(intNumber))
+
+# how to find floating number
+
+floatNumber = 50.5
+print(type(floatNumber))
+
+
+# how to find complex type data
+
+complexDataType = 589674j
+
+print(type(complexDataType))
+
+
+#how to find string type data
+
+name = "Bishwajit Aich"
+
+print(type(name))
+
+# String Type Data
+
+firstName = 'Bishwajit'
+lastName = 'Aich'
+
+print('My Name Is : ',firstName + ' ' + lastName)
+
+# How to find Boolean Datatype
+
+bool = False
+
+print(bool)
+
+x = 100
+y = 90
+
+print(x > y)
